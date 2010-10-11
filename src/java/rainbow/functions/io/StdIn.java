3377741f1624104dd8689e1fcce67459c3ecc86f
conanite <conan@conandalton.net>
2010-10-11 13:52:08 +0200

    fixed broken (stdin)

--- src/java/rainbow/functions/io/StdIn.java
+++ src/java/rainbow/functions/io/StdIn.java
@@ -4,14 +4,19 @@ import rainbow.functions.Builtin;
 import rainbow.functions.IO;
 import rainbow.types.ArcObject;
 import rainbow.types.Pair;
-import rainbow.LexicalClosure;
+import rainbow.vm.VM;
 
 public class StdIn extends Builtin {
   public StdIn() {
     super("stdin");
   }
 
-  public ArcObject invoke(LexicalClosure lc, Pair args) {
+  public void invokef(VM vm) {
+    vm.pushA(IO.stdIn());
+  }
+
+  public ArcObject invoke(Pair args) {
+    checkExactArgsCount(args, 0, getClass());
     return IO.stdIn();
   }
 }
